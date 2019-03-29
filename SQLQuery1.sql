7e0a36a2b7cbe7456c16946c6491bd2c1357cfc5
marcinwat <marcin.sienicki@student.wat.edu.pl>
2019-03-29 16:08:06 +0100

    zmiana 1

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,21 +1,22 @@
-CREATE TABLE Drukarki (
-    id_Drukarki int PRIMARY KEY NOT NULL IDENTITY(1,1),
-    model nvarchar(20),
+use master;
+drop database FirmaKomputerowa;
+create database FirmaKomputerowa;
+
+CREATE TABLE [FirmaKomputerowa].[dbo].[Drukarki] (
+    model nvarchar(20) PRIMARY KEY,
 	kolor bit,
 	typ nvarchar(20),
 	cena int,
 ); 
 
-CREATE TABLE Produkty (
-    id_Produktu int PRIMARY KEY NOT NULL IDENTITY(1,1),
+CREATE TABLE [FirmaKomputerowa].[dbo].[Produkty] (
     producent nvarchar(20),
-	model nvarchar(20),
+	model nvarchar(20) PRIMARY KEY,
 	typ nvarchar(20),
 ); 
 
-CREATE TABLE Laptop (
-    id_Laptopa int PRIMARY KEY NOT NULL IDENTITY(1,1),
-    model nvarchar(20),
+CREATE TABLE [FirmaKomputerowa].[dbo].[Laptop] (
+    model nvarchar(20) PRIMARY KEY,
     procesor int,
 	ram int,
     hdd int,
@@ -23,9 +24,8 @@ CREATE TABLE Laptop (
 	cena int,
 ); 
 
-CREATE TABLE PC (
-    id_PC int PRIMARY KEY NOT NULL IDENTITY(1,1),
-    model nvarchar(20),
+CREATE TABLE [FirmaKomputerowa].[dbo].[PC] (
+    model nvarchar(20) PRIMARY KEY,
     procesor int,
 	ram int,
     hdd int,
