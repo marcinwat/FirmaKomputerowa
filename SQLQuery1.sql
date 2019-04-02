38a0b8797be2b9d1b911943d1edf8f0b574016fe
marcinwat <marcin.sienicki@student.wat.edu.pl>
2019-04-02 17:54:53 +0200

    jeszcze 13

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -77,3 +77,90 @@ insert into Drukarki values('HL-1223WE', 0, 'laserowa', 419);
 insert into Drukarki values('B412dn', 0, 'laserowa', 529);
 insert into Drukarki values('EcoTank ITS L3050', 1, 'atramentowa', 699);
 insert into Drukarki values('EcoTank L3070', 1, 'atramentowa', 829);
+
+
+-- zadanie 1 
+
+SELECT 'Z1'[Zadanie], model, procesor, hdd 
+FROM PC
+WHERE cena < 2000 ;
+
+-- zadanie 2
+
+SELECT 'Z2' [zadanie], producent
+FROM Produkty
+WHERE typ = 'Drukarka';
+
+-- zadanie 3
+
+SELECT 'Z3'[Zadanie],model, ram, ekran 
+FROM PC
+WHERE cena > 2000 ;
+
+-- zadanie 4
+SELECT 'Z4'[Zadanie],model, kolor, typ, cena 
+FROM Drukarki
+WHERE kolor = 1;
+
+-- zadanie 5
+
+SELECT 'Z5'[Zadanie],model, procesor, hdd
+FROM PC
+WHERE (cd = 8 and cena < 2500) or (cd = 16 and cena > 2500);
+
+-- zadanie 6
+
+SELECT 'Z6'[Zadanie],Prod.producent, Lap.procesor
+FROM Produkty AS Prod
+JOIN Laptopy AS Lap
+ON Prod.model = Lap.model
+WHERE Lap.hdd >= 500;
+
+
+-- zadanie 7
+
+SELECT 'Z7'[Zadanie],Prod.producent, PC.procesor
+FROM Produkty AS Prod
+JOIN PC
+ON Prod.model = PC.model
+WHERE PC.procesor >= 3500;
+
+-- zadanie 8
+
+SELECT 'Z8'[Zadanie],model, cena
+FROM Drukarki
+WHERE cena = (SELECT MAX(cena) FROM Drukarki);
+
+-- zadanie 9 
+
+SELECT 'Z9'[Zadanie],AVG(procesor) AS [Œrednia prêdkoœæ procesora]
+FROM PC;
+
+-- zadanie 10
+
+SELECT 'Z10'[Zadanie],AVG(procesor) AS [Œrednia prêdkoœæ procesora o cenie powyzej 3500]
+FROM PC WHERE cena > 3500 ;
+ 
+ -- zadanie 11 
+
+SELECT 'Z11'[Zadanie],AVG(PC.procesor) AS [Œrednia prêdkoœæ procesora DELL]
+FROM Produkty AS Prod
+join PC
+ON Prod.model = PC.model
+WHERE Prod.producent like('DELL');
+
+-- zadanie 12
+
+SELECT 'Z12'[Zadanie],procesor,AVG(cena) AS 'srednia cena'
+FROM PC
+GROUP BY procesor
+
+-- ZADANIE 13
+
+SELECT 'Z13' [Zadanie], cast(Prod.producent as varchar(50)) as 'Producent', AVG(Lap.ekran) AS [sredni ekran]
+FROM Produkty AS Prod
+JOIN Laptopy AS Lap
+ON Prod.model = Lap.model
+GROUP BY CAST(Prod.producent AS VARCHAR(50))
+ORDER BY [sredni ekran] DESC
+
